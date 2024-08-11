7b2ee10cd1b6881905c491ccaa4c03c8081ff693
Cleyson <cleysonph@gmail.com>
2024-08-11 01:18:55 -0300

    initial config

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { app } from './app'
+import { env } from './env'
+
+app.get('/api/ping', () => {
+  return { message: 'pong' }
+})
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log(`🚀 Server is running on port ${env.PORT}`)
+  })
